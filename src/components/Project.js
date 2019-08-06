4441aee6a335f2037f0b442e7f5fce9e5d3f9473
Javontay McElroy <dinomightnetwork@gmail.com>
2019-08-06 12:15:22 -0500

    Add media queries.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { Component } from "react";
+import React from 'react';
+import { Component } from 'react';
 
-import "./Project.scss";
+import './Project.scss';
+import './ProjectsMobileResponsive.scss';
 
 class Project extends Component {
   constructor(props) {
@@ -16,30 +17,30 @@ class Project extends Component {
       project => `${project.id}` === this.state.id
     );
     return (
-      <div className="project-container">
+      <div className='project-container'>
         <img
           src={projectsArray.projectImg}
           alt={projectsArray.projectName}
-          className="single-project-image"
+          className='single-project-image'
         />
-        <div className="project-content">
+        <div className='project-content'>
           <h1>{projectsArray.projectName}</h1>
           <h4>{projectsArray.projectRole}</h4>
           <p>{projectsArray.projectDesc}</p>
           <h4>Tech Stack: {projectsArray.languages}</h4>
-          <div className="button-flex">
+          <div className='button-flex'>
             <a
               href={projectsArray.projectUrl}
-              target="_blank"
-              rel="noopener noreferrer"
+              target='_blank'
+              rel='noopener noreferrer'
             >
               Visit
             </a>
             {projectsArray.githubUrl ? (
               <a
                 href={projectsArray.githubUrl}
-                target="_blank"
-                rel="noopener noreferrer"
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 Github
               </a>
